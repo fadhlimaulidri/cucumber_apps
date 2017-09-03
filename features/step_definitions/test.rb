476c09053477c241d2324303174287c156dc224b
Fadhli Maulidri <fadhli@bukalapak.com>
2017-09-03 11:56:08 +0700

    environment

--- features/step_definitions/test.rb
+++ features/step_definitions/test.rb
@@ -1,5 +1,5 @@
 Given(/^there is "(.*?)" who have been registered before$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  visit('m.bukalapak.com')
 end
 
 When(/^visit home page$/) do
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,11 @@
+require 'rspec/expectations'
+require 'appium_lib'
+require 'cucumber/ast'
+
 caps = Appium.load_appium_txt(file:File.expand_path('./',__FILE__))
 $driver = Appium::Driver.new(caps)
 
-before{
+Before{
   @browser = $driver.start_driver
   @browser.get('http://google.com')
 }
